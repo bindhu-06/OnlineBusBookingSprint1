c433aeb98ed58e21ab92ffd7a03ddea3918c5269
bindhu-06 <82869785+bindhu-06@users.noreply.github.com>
2021-04-21 15:47:16 +0530

    Update BusRoute.java
    new

--- OnlineBusBooking/src/main/java/com/cg/entities/BusRoute.java
+++ OnlineBusBooking/src/main/java/com/cg/entities/BusRoute.java
@@ -1,10 +1,10 @@
 package com.cg.entities;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
@@ -12,42 +12,73 @@ import javax.persistence.Table;
 public class BusRoute {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	private  int busrouteId;
 	private String routeName ;
 	private String source;
 	private String destination;
-	public BusRoute(String routeName, String source, String destination) {
+	
+	@OneToOne(mappedBy="busRoute")
+  
+	private Bus bus;
+
+	public BusRoute(int busrouteId, String routeName, String source, String destination, Bus bus) {
 		super();
+		this.busrouteId = busrouteId;
 		this.routeName = routeName;
 		this.source = source;
 		this.destination = destination;
+		this.bus = bus;
 	}
+
 	public BusRoute() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	@OneToOne(mappedBy="busRoute", cascade=CascadeType.ALL)
-	private Bus bus;
+
+	public int getBusrouteId() {
+		return busrouteId;
+	}
+
+	public void setBusrouteId(int busrouteId) {
+		this.busrouteId = busrouteId;
+	}
+
 	public String getRouteName() {
 		return routeName;
 	}
+
 	public void setRouteName(String routeName) {
 		this.routeName = routeName;
 	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
+
 	public String getDestination() {
 		return destination;
 	}
+
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
-	@Override
-	public String toString() {
-		return "BusRoute [routeName=" + routeName + ", source=" + source + ", destination=" + destination + "]";
+
+	public Bus getBus() {
+		return bus;
 	}
 
+	public void setBus(Bus bus) {
+		this.bus = bus;
+	}
+
+	@Override
+	public String toString() {
+		return "BusRoute [busrouteId=" + busrouteId + ", routeName=" + routeName + ", source=" + source
+				+ ", destination=" + destination + ", bus=" + bus + "]";
+	}
+	
 }
