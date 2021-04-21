1699ec29b0ca64147c4ee15c3725bdb76ce6c500
bindhu-06 <82869785+bindhu-06@users.noreply.github.com>
2021-04-21 15:44:52 +0530

    new

--- OnlineBusBooking/src/main/java/com/cg/entities/Bus.java
+++ OnlineBusBooking/src/main/java/com/cg/entities/Bus.java
@@ -1,10 +1,11 @@
 package com.cg.entities;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
@@ -12,38 +13,52 @@ import javax.persistence.Table;
 public class Bus {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-private String busNumber;
+private int busId;
+	
+	private String busNumber;
+	@OneToOne
+	@JoinColumn(name="busrouteId")
 	private BusRoute busRoute;
 	private int totalSeats;
 	private int fare;
-	
-	
-	public Bus(String busNumber, BusRoute busRoute, int totalSeats, int fare) {
+	@ManyToOne
+	@JoinColumn(name="busoperatorId")
+	private  BusOperator busOperator;
+	@OneToOne
+	@JoinColumn(name="busoperatorrequestId")
+	private BusOperatorRequest busOperatorRequest;
+	public Bus(int busId, String busNumber, BusRoute busRoute, int totalSeats, int fare, BusOperator busOperator,
+			BusOperatorRequest busOperatorRequest) {
 		super();
+		this.busId = busId;
 		this.busNumber = busNumber;
 		this.busRoute = busRoute;
 		this.totalSeats = totalSeats;
 		this.fare = fare;
-		
-	}
-	public Bus(BusRoute busRoute) {
-		super();
-		this.busRoute = busRoute;
+		this.busOperator = busOperator;
+		this.busOperatorRequest = busOperatorRequest;
 	}
 	public Bus() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	@OneToOne(mappedBy="bus", cascade=CascadeType.ALL)
-	
-
-	private BusOperatorRequest busoperatorrequest;
-	
+	public int getBusId() {
+		return busId;
+	}
+	public void setBusId(int busId) {
+		this.busId = busId;
+	}
 	public String getBusNumber() {
 		return busNumber;
 	}
 	public void setBusNumber(String busNumber) {
 		this.busNumber = busNumber;
+	}
+	public BusRoute getBusRoute() {
+		return busRoute;
+	}
+	public void setBusRoute(BusRoute busRoute) {
+		this.busRoute = busRoute;
 	}
 	public int getTotalSeats() {
 		return totalSeats;
@@ -57,15 +72,23 @@ private String busNumber;
 	public void setFare(int fare) {
 		this.fare = fare;
 	}
+	public BusOperator getBusOperator() {
+		return busOperator;
+	}
+	public void setBusOperator(BusOperator busOperator) {
+		this.busOperator = busOperator;
+	}
+	public BusOperatorRequest getBusOperatorRequest() {
+		return busOperatorRequest;
+	}
+	public void setBusOperatorRequest(BusOperatorRequest busOperatorRequest) {
+		this.busOperatorRequest = busOperatorRequest;
+	}
 	@Override
 	public String toString() {
-		return "Bus [busNumber=" + busNumber + ", totalSeats=" + totalSeats + ", fare=" + fare + "]";
+		return "Bus [busId=" + busId + ", busNumber=" + busNumber + ", busRoute=" + busRoute + ", totalSeats="
+				+ totalSeats + ", fare=" + fare + ", busOperator=" + busOperator + ", busOperatorRequest="
+				+ busOperatorRequest + "]";
 	}
-	public BusRoute getBusRoute() {
-		return busRoute;
-	}
-	public void setBusRoute(BusRoute busRoute) {
-		this.busRoute = busRoute;
-	}
-
+	
 }
